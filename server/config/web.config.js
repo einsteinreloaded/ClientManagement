9f2c1d12215180ea76135fd978aa6e93d7e3ae0c
Anish Kumar <changayil1994@gmail.com>
2017-10-03 14:25:08 +0530

    removing web config js from commit in future

--- server/config/web.config.js
+++ server/config/web.config.js
@@ -2,11 +2,11 @@
     dbUrl: "http://localhost:8800/users.json",
 
     firebaseConfig: {// replace with firebase db config here
-        apiKey: 'xxxxxx',
-        authDomain: 'xxxx',
-        databaseURL: 'xxx',
-        storageBucket: 'xxx',
-        messagingSenderId: 'xxx'
+        apiKey: 'AIzaSyDdvcBo3ixIUKjHPxjBC5y3vkY54yM_KqU',
+        authDomain: 'totally-awesome-c947c.firebaseapp.com',
+        databaseURL: 'https://totally-awesome-c947c.firebaseio.com',
+        storageBucket: 'totally-awesome-c947c.appspot.com',
+        messagingSenderId: '220706728204'
     }
  }
 module.exports = server_config;
